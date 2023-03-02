d3a8251e4411f8b2642cb534455666e43725676d
HugoMorales98 <124092671+HugoMorales98@users.noreply.github.com>
2023-03-02 00:27:18 -0600

    test-challenge-practica
    Practica 2 de marzo del 2023

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,22 +3,22 @@ const { default: expect } = require('expect');
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
-    //
+    expect(calculator.sum(2, 3)).toBe(5);//
 })
 
 test('substracts a - b', () => {
-   //
+    expect(calculator.substract(5, 3)).toBe(2);// //
 })
 
 test('multiplies a * b', () => {
-    //
+    expect(calculator.multiply(2, 3)).toBe(6);//
 })
 
 test('divides a / b', () => {
-    // Consider decimals
+    expect(calculator.divide(6, 3)).toBe(2);// Consider decimals
 })
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(calculator.power(4, 6)).toBe(4096);//
 })
 
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,9 +1,21 @@
 // You already have the methods imported
 const { default: expect } = require('expect');
 const {getUser, getUsers} = require('../users');
-
+const users = require('../data');
 
 // Make a test to verify if the method getUser(id) works correctly
+test('get user by id', () => {
+  const user = getUser(3);
+  expect(user).toEqual(users[2]);
+});
+
 // Verify any data of the resulting object you want
+test('get all users', () => {
+  const allUsers = getUsers();
+  expect(allUsers).toEqual(users);
+});
+
+
+
 
 // READ documentation
